a038bb99535fb02c6e850b359d16b65643d2423f
FifiBm <fifibm90@gmail.com>
2022-01-10 16:34:46 -0800

    this is the first work JS

---
+++ index.js
@@ -0,0 +1,9 @@
+var camelCase;
+// declare another variable
+
+var name;
+var initVariable = 0;
+// initialize 2 more variables with one being a boolean and the other a string
+
+var number = '7';
+var bottomtext = false;
